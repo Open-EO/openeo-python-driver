f0eca375dcb217b332a2193b57af6df6ff353ed8
driesj <jeroen.dries@vito.be>
2017-12-05 16:58:52 +0100

    moved generic python driver files to separate repo

---
+++ openeo_driver/ProcessGraphDeserializer.py
@@ -0,0 +1,58 @@
+import base64
+import json
+import pickle
+from typing import Dict
+
+from openeo import ImageCollection
+import os
+import importlib
+
+def getImageCollection(product_id:str, viewingParameters):
+    raise "Please provide getImageCollection method in your base package."
+
+i = importlib.import_module(os.getenv('DRIVER_IMPLEMENTATION_PACKAGE', "openeogeotrellis"))
+
+
+
+
+def graphToRdd(processGraph:Dict, viewingParameters)->ImageCollection:
+    if 'product_id' in processGraph:
+        return getImageCollection(processGraph['product_id'],viewingParameters)
+    elif 'process_id' in processGraph:
+        return getProcessImageCollection(processGraph['process_id'],processGraph['args'],viewingParameters)
+    else:
+        raise AttributeError("Process should contain either product_id or process_id, but got: \n" + json.dumps(processGraph,indent=1))
+
+
+def extract_arg(args:Dict,name:str)->str:
+    try:
+        return args[name]
+    except KeyError:
+        raise AttributeError(
+            "Required argument " +name +" should not be null in band_arithmetic. Arguments: \n" + json.dumps(args,indent=1))
+
+
+def band_arithmetic(input_collection:ImageCollection, args:Dict, viewingParameters)->ImageCollection:
+    function = extract_arg(args,'function')
+    bands = extract_arg(args,'bands')
+    decoded_function = pickle.loads(base64.standard_b64decode(function))
+    return input_collection.combinebands(bands,decoded_function)
+
+
+def reduce_by_time(input_collection:ImageCollection, args:Dict, viewingParameters)->ImageCollection:
+    function = extract_arg(args,'function')
+    temporal_window = extract_arg(args,'temporal_window')
+    decoded_function = pickle.loads(base64.standard_b64decode(function))
+    return input_collection.reduceByTime(temporal_window,decoded_function)
+
+
+def getProcessImageCollection( process_id:str, args:Dict, viewingParameters)->ImageCollection:
+
+    imagery = extract_arg(args,'imagery')
+    child_collection = graphToRdd(imagery,viewingParameters)
+
+    print(globals().keys())
+    process_function = globals()[process_id]
+    if process_function is None:
+        raise RuntimeError("No process found with name: "+process_id)
+    return process_function(child_collection,args,viewingParameters)
