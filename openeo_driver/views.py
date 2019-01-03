fd0c828585593e95fcee288a4ab76e860c132624
Jeroen Dries <jeroen.dries@vito.be>
2019-01-03 11:41:51 +0100

    add multi-version support to python driver

--- openeo_driver/views.py
+++ openeo_driver/views.py
@@ -1,8 +1,9 @@
 import os
 import logging
+from urllib.parse import unquote
 
-from flask import request, url_for, jsonify, send_from_directory, abort, make_response
-from werkzeug.exceptions import HTTPException
+from flask import request, url_for, jsonify, send_from_directory, abort, make_response,Blueprint,g
+from werkzeug.exceptions import HTTPException, BadRequest
 
 from openeo_driver import app
 from .ProcessGraphDeserializer import (evaluate, health_check, get_layers, getProcesses, getProcess, get_layer,
@@ -10,10 +11,35 @@ from .ProcessGraphDeserializer import (evaluate, health_check, get_layers, getPr
                                        get_batch_job_result_filenames, get_batch_job_result_output_dir)
 from openeo import ImageCollection
 
-ROOT = '/openeo'
+SUPPORTED_VERSIONS = ['0.3.0', '0.4.0']
+
+openeo_bp = Blueprint('openeo', __name__)
 
 
-@app.errorhandler(HTTPException)
+@openeo_bp.url_defaults
+def add_language_code(endpoint, values):
+    if 'version' in values or not g.version:
+        return
+    if app.url_map.is_endpoint_expecting(endpoint, 'version'):
+        values['version'] = g.version
+    else:
+        values['version'] = '0.3.0'
+
+@openeo_bp.url_value_preprocessor
+def pull_version(endpoint, values):
+    g.version = values.pop('version', '0.3.0')
+    if g.version not in SUPPORTED_VERSIONS:
+        error = HTTPException(response={
+            "id": "550e8400-e29b-11d4-a716-446655440000",
+            "code": 400,
+            "message": "Unsupported version: " + g.version + ".  Supported versions: " + str(SUPPORTED_VERSIONS)
+        })
+        error.code = 400
+        raise error
+
+
+
+@openeo_bp.errorhandler(HTTPException)
 def handle_http_exceptions(error: HTTPException):
     return _error_response(error, error.code)
 
@@ -27,45 +53,46 @@ def _error_response(error: Exception, status_code: int):
     error_json = {
         "message":str(error)
     }
+    if type(error) is HTTPException and type(error.response) is dict:
+        error_json = error.response
     import traceback
     print(traceback.format_exception(None,error,error.__traceback__))
     response = jsonify(error_json)
     response.status_code = status_code
     return response
 
-
-@app.route('%s' % ROOT)
+@openeo_bp.route('/' )
 def index():
     return jsonify({
-      "version": "0.3.0",
+      "version": g.version,
       "endpoints": [
         {
-          "path": "%s/data"%ROOT,
+          "path": url_for('.data'),
           "methods": [
             "GET"
           ]
         },
         {
-          "path": "%s/data/{data_id}"%ROOT,
+          "path": url_for('.data') + '/{data_id}',
           "methods": [
             "GET"
           ]
         },
         {
-          "path": "%s/jobs"%ROOT,
+          "path": url_for('.create_job'),
           "methods": [
             "GET",
             "POST"
           ]
         },
           {
-              "path": "%s/processes"%ROOT,
+              "path": url_for('.processes'),
               "methods": [
                   "GET"
               ]
           },
         {
-          "path": "%s/jobs/{job_id}"%ROOT,
+          "path": unquote(url_for('.get_job_info', job_id = '{job_id}')),
           "methods": [
             "GET",
             "DELETE",
@@ -85,11 +112,11 @@ def index():
       }
     })
 
-@app.route('%s/health' % ROOT)
+@openeo_bp.route('/health' )
 def health():
     return health_check()
 
-@app.route('%s/capabilities' % ROOT)
+@openeo_bp.route('/capabilities')
 def capabilities():
     return jsonify([
       "/data",
@@ -99,9 +126,9 @@ def capabilities():
 
 
 #deprecated:
-@app.route('%s/capabilities/output_formats' % ROOT)
+@openeo_bp.route('/capabilities/output_formats' )
 #OpenEO 0.3.0:
-@app.route('%s/output_formats' % ROOT)
+@openeo_bp.route('/output_formats' )
 def output_formats():
     return jsonify({
       "default": "GTiff",
@@ -113,12 +140,12 @@ def output_formats():
 
 
 
-@app.route('%s/timeseries' % ROOT)
+@openeo_bp.route('/timeseries' )
 def timeseries():
-    return 'OpenEO GeoPyspark backend. ' + url_for('point')
+    return 'OpenEO GeoPyspark backend. ' + url_for('.point')
 
 
-@app.route('%s/timeseries/point' % ROOT, methods=['GET', 'POST'])
+@openeo_bp.route('/timeseries/point' , methods=['GET', 'POST'])
 def point():
     if request.method == 'POST':
         print("Handling request: "+str(request))
@@ -136,7 +163,7 @@ def point():
         return 'Usage: Query point timeseries using POST.'
 
 
-@app.route('%s/download' % ROOT, methods=['GET', 'POST'])
+@openeo_bp.route('/download' , methods=['GET', 'POST'])
 def download():
     if request.method == 'POST':
         print("Handling request: "+str(request))
@@ -152,7 +179,7 @@ def download():
         return 'Usage: Download image using POST.'
 
 
-@app.route('%s/execute' % ROOT, methods=['GET', 'POST'])
+@openeo_bp.route('/execute' , methods=['GET', 'POST'])
 def execute():
     if request.method == 'POST':
         print("Handling request: "+str(request))
@@ -174,7 +201,7 @@ def execute():
         return 'Usage: Directly evaluate process graph using POST.'
 
 
-@app.route('%s/jobs' % ROOT, methods=['GET', 'POST'])
+@openeo_bp.route('/jobs', methods=['GET', 'POST'])
 def create_job():
     if request.method == 'POST':
         print("Handling request: "+str(request))
@@ -195,13 +222,13 @@ def create_job():
         return 'Usage: Create a new batch processing job using POST'
 
 
-@app.route('%s/jobs/<job_id>' % ROOT, methods=['GET'])
+@openeo_bp.route('/jobs/<job_id>' , methods=['GET'])
 def get_job_info(job_id):
     job_info = get_batch_job_info(job_id)
     return jsonify(job_info) if job_info else abort(404)
 
 
-@app.route('%s/jobs/<job_id>/results' % ROOT, methods=['POST'])
+@openeo_bp.route('/jobs/<job_id>/results' , methods=['POST'])
 def queue_job(job_id):
     print("Handling request: " + str(request))
 
@@ -214,7 +241,7 @@ def queue_job(job_id):
         abort(404)
 
 
-@app.route('%s/jobs/<job_id>/results' % ROOT, methods=['GET'])
+@openeo_bp.route('/jobs/<job_id>/results' , methods=['GET'])
 def list_job_results(job_id):
     print("Handling request: " + str(request))
 
@@ -230,7 +257,7 @@ def list_job_results(job_id):
         return abort(404)
 
 
-@app.route('%s/jobs/<job_id>/results/<filename>' % ROOT, methods=['GET'])
+@openeo_bp.route('/jobs/<job_id>/results/<filename>' , methods=['GET'])
 def get_job_result(job_id, filename):
     print("Handling request: " + str(request))
 
@@ -240,7 +267,7 @@ def get_job_result(job_id, filename):
 #SERVICES API https://open-eo.github.io/openeo-api/v/0.3.0/apireference/#tag/Web-Service-Management
 
 
-@app.route('%s/service_types' % ROOT)
+@openeo_bp.route('/service_types' )
 def service_types():
     return jsonify({
   "WMTS": {
@@ -268,7 +295,7 @@ def service_types():
   }
 })
 
-@app.route('%s/tile_service' % ROOT, methods=['GET', 'POST'])
+@openeo_bp.route('/tile_service' , methods=['GET', 'POST'])
 def tile_service():
     """
     This is deprecated, pre-0.3.0 API
@@ -283,7 +310,7 @@ def tile_service():
     else:
         return 'Usage: Retrieve tile service endpoint.'
 
-@app.route('%s/services' % ROOT, methods=['GET', 'POST'])
+@openeo_bp.route('/services' , methods=['GET', 'POST'])
 def services():
     """
     GET: Requests to this endpoint will list all running secondary web services submitted by a user with given id.
@@ -303,12 +330,12 @@ def services():
 
         if 'tms' == type.lower():
             image_collection = evaluate(process_graph)
-            return jsonify(image_collection.tiled_viewing_service(**json_request)),201, {'ContentType':'application/json','Location':url_for('services')}
+            return jsonify(image_collection.tiled_viewing_service(**json_request)),201, {'ContentType':'application/json','Location':url_for('.services')}
         if 'wmts' == type.lower():
             image_collection = evaluate(process_graph,viewingParameters={
                 'service_type':type
             })
-            return jsonify(image_collection.tiled_viewing_service(**json_request)),201, {'ContentType':'application/json','Location':url_for('services')}
+            return jsonify(image_collection.tiled_viewing_service(**json_request)),201, {'ContentType':'application/json','Location':url_for('.services')}
         else:
             raise NotImplementedError("Requested unsupported service type: " + type)
     elif request.method == 'GET':
@@ -317,19 +344,19 @@ def services():
     else:
         abort(405)
 
-@app.route('%s/data' % ROOT, methods=['GET'])
+@openeo_bp.route('/data' , methods=['GET'])
 def data():
         print("Handling request: "+str(request))
         layers = get_layers()
         return jsonify(layers)
 
-@app.route('%s/data/<collection_id>' % ROOT, methods=['GET'])
+@openeo_bp.route('/data/<collection_id>' , methods=['GET'])
 def collection(collection_id):
     print("Handling request: "+str(request))
     return jsonify(get_layer(collection_id))
 
 
-@app.route('%s/processes' % ROOT, methods=['GET'])
+@openeo_bp.route('/processes' , methods=['GET'])
 def processes():
     print("Handling request: " + str(request))
 
@@ -338,7 +365,7 @@ def processes():
     return jsonify(getProcesses(substring))
 
 
-@app.route('%s/processes/<process_id>' % ROOT, methods=['GET'])
+@openeo_bp.route('/processes/<process_id>' , methods=['GET'])
 def process(process_id):
     print("Handling request: " + str(request))
 
@@ -346,3 +373,6 @@ def process(process_id):
     print(process_details)
 
     return jsonify(process_details) if process_details else abort(404)
+
+app.register_blueprint(openeo_bp, url_prefix='/openeo')
+app.register_blueprint(openeo_bp, url_prefix='/openeo/<version>')
