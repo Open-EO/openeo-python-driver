ca287da441ddd9d775a3595de9bbfd253bcacc65
Stefaan Lippens <stefaan.lippens@vito.be>
2020-04-06 17:55:31 +0200

    to_netcdf tests: write to temp output folder  (with tmp_path fixture)

--- tests/test_save_result_netcdf.py
+++ tests/test_save_result_netcdf.py
@@ -6,7 +6,7 @@ from openeo_driver.save_result import AggregatePolygonResult
 from shapely.geometry import GeometryCollection, Polygon
 
 
-def test_aggregate_polygon_result_basic():
+def test_aggregate_polygon_result_basic(tmp_path):
     timeseries = {
         "2019-10-15T08:15:45Z": [[1, 2, 3], [4, 5, 6]],
         "2019-11-11T01:11:11Z": [[7, 8, 9], [10, 11, 12]],
@@ -19,7 +19,7 @@ def test_aggregate_polygon_result_basic():
     result = AggregatePolygonResult(timeseries, regions=regions)
     result.set_format("netcdf")
 
-    filename = result.to_netcdf('timeseries_xarray.nc')
+    filename = result.to_netcdf(tmp_path / 'timeseries_xarray.nc')
 
     timeseries_ds = xr.open_dataset(filename)
     print(timeseries_ds)
@@ -29,7 +29,7 @@ def test_aggregate_polygon_result_basic():
     print(timeseries_ds)
 
 
-def test_aggregate_polygon_result_nan_values():
+def test_aggregate_polygon_result_nan_values(tmp_path):
     timeseries = {
         "2019-10-15T08:15:45Z": [[1, 2, 3], [4, np.nan, 6]],
         "2019-11-11T01:11:11Z": [[7, 8, 9], [np.nan, np.nan, np.nan]],
@@ -41,14 +41,14 @@ def test_aggregate_polygon_result_nan_values():
 
     result = AggregatePolygonResult(timeseries, regions=regions)
 
-    filename = result.to_netcdf('timeseries_xarray_nodata.nc')
+    filename = result.to_netcdf(tmp_path / 'timeseries_xarray_nodata.nc')
     timeseries_ds = xr.open_dataset(filename)
     assert_array_equal(timeseries_ds.band_0.sel(feature=0).data, [1,7])
     assert_array_equal(timeseries_ds.band_2.sel(feature=0).data, [3, 9])
     assert_array_equal(timeseries_ds.band_2.sel(feature=1).data, [6, np.nan])
 
 
-def test_aggregate_polygon_result_empty_ts_data():
+def test_aggregate_polygon_result_empty_ts_data(tmp_path):
     timeseries = {
         "2019-01-01T12:34:56Z": [[1, 2], [3, 4]],
         "2019-02-01T12:34:56Z": [[5, 6], []],
@@ -64,7 +64,7 @@ def test_aggregate_polygon_result_empty_ts_data():
     result = AggregatePolygonResult(timeseries, regions=regions)
     result.set_format("netcdf")
 
-    filename = result.to_netcdf('timeseries_xarray_empty.nc')
+    filename = result.to_netcdf(tmp_path / 'timeseries_xarray_empty.nc')
     timeseries_ds = xr.open_dataset(filename)
 
     assert_array_equal(timeseries_ds.time.data, [
@@ -78,7 +78,7 @@ def test_aggregate_polygon_result_empty_ts_data():
 
 
 
-def test_aggregate_polygon_result_inconsistent_bands():
+def test_aggregate_polygon_result_inconsistent_bands(tmp_path):
     timeseries = {
         "2019-01-01T12:34:56Z": [[1, 2], [3, 4, 5]],
         "2019-02-01T12:34:56Z": [[6], []],
@@ -92,4 +92,4 @@ def test_aggregate_polygon_result_inconsistent_bands():
     result.set_format("netcdf")
 
     with pytest.raises(ValueError):
-        filename = result.to_netcdf('timeseries_xarray_invalid.nc')
+        filename = result.to_netcdf(tmp_path / 'timeseries_xarray_invalid.nc')
