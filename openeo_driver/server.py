f46f5eb3057c2444c60d1a4c0f77f0326e8339cf
Jan Van den bosch <jan@bossie.org>
2020-06-22 08:23:01 +0200

    EP-3382: added reusable server, adapted local_app to

--- openeo_driver/server.py
+++ openeo_driver/server.py
@@ -1,40 +1,72 @@
+from flask_cors import CORS
 import gunicorn.app.base
 from gunicorn.six import iteritems
-
-from openeo_driver.views import app
-
-"""
-Script to start a production server. This script can serve as the entry-point for doing spark-submit.
-"""
+import logging
+from typing import Union
 
 
-def number_of_workers():
-    return 4#(multiprocessing.cpu_count() * 2) + 1
+def show_log_level(logger: logging.Logger):
+    """Helper to show threshold log level of a logger."""
+    level = logger.getEffectiveLevel()
+    logger.log(level, 'Logger {n!r} level: {t}'.format(n=logger.name, t=logging.getLevelName(level)))
+
+
+def run(title: str, description: str, deploy_metadata: Union[dict, None], backend_version: str, threads: int, host: str, port: int, on_started=lambda: None) -> None:
+    """ Starts a web server exposing OpenEO-GeoPySpark, bound to a public IP. """
+
+    from openeo_driver.views import app
+    CORS(app)
+
+    app.logger.setLevel('DEBUG')
+    app.config['OPENEO_BACKEND_VERSION'] = backend_version
+    app.config['OPENEO_TITLE'] = title
+    app.config['OPENEO_DESCRIPTION'] = description
+    app.config['OPENEO_BACKEND_DEPLOY_METADATA'] = deploy_metadata
+
+    app.logger.info('App info logging enabled!')
+    app.logger.debug('App debug logging enabled!')
+
+    #note the use of 1 worker and multiple threads
+    # we were seeing strange py4j errors when executing multiple requests in parallel
+    # this seems to be related by the type and configuration of worker that gunicorn uses, aiohttp also gave very bad results
+    options = {
+        'bind': '%s:%s' % (host, port),
+        'workers': 1,
+        'threads': threads,
+        'worker_class': 'gthread',
+        'timeout': 1000,
+        'loglevel': 'DEBUG',
+        'accesslog': '-',
+        'errorlog': '-'
+    }
+
+    print(options)
+
+    def when_ready(server) -> None:
+        print(server)
+
+        logging.getLogger('gunicorn.error').info('Gunicorn info logging enabled!')
+        logging.getLogger('flask').info('Flask info logging enabled!')
+
+        on_started()
+
+    application = StandaloneApplication(app, when_ready, options)
+    application.run()
 
 
 class StandaloneApplication(gunicorn.app.base.BaseApplication):
-
-    def __init__(self, app, options=None):
+    def __init__(self, app, when_ready, options=None):
         self.options = options or {}
         self.application = app
+        self.when_ready = when_ready
         super(StandaloneApplication, self).__init__()
 
     def load_config(self):
         config = dict([(key, value) for key, value in iteritems(self.options)
                        if key in self.cfg.settings and value is not None])
+        config['when_ready'] = self.when_ready
         for key, value in iteritems(config):
             self.cfg.set(key.lower(), value)
 
     def load(self):
         return self.application
-
-
-if __name__ == '__main__':
-    options = {
-        'bind': '%s:%s' % ('127.0.0.1', '0'),
-        'workers': number_of_workers(),
-        'worker_class':'gaiohttp',
-        'timeout':1000
-    }
-    # Modification 3: pass Flask app instead of handler_app
-    StandaloneApplication(app, options).run()
