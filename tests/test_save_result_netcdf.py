ee34c0ea087d749abc12b965af33f80c5547aad7
Jeroen Dries <jeroen.dries@vito.be>
2021-04-06 17:04:19 +0200

    timeseries export to netcdf was not exposing the correct timestamp for a given value, use 't' for time dimension, like in the openEO dimension name

--- tests/test_save_result_netcdf.py
+++ tests/test_save_result_netcdf.py
@@ -8,8 +8,8 @@ from shapely.geometry import GeometryCollection, Polygon
 
 def test_aggregate_polygon_result_basic(tmp_path):
     timeseries = {
-        "2019-10-15T08:15:45Z": [[1, 2, 3], [4, 5, 6]],
         "2019-11-11T01:11:11Z": [[7, 8, 9], [10, 11, 12]],
+        "2019-10-15T08:15:45Z": [[1, 2, 3], [4, 5, 6]],
     }
     regions = GeometryCollection([
         Polygon([(0, 0), (5, 1), (1, 4)]),
@@ -23,10 +23,11 @@ def test_aggregate_polygon_result_basic(tmp_path):
 
     timeseries_ds = xr.open_dataset(filename)
     print(timeseries_ds)
-    assert_array_equal(timeseries_ds.band_0.coords['time'].data, np.asarray([ np.datetime64('2019-10-15T08:15:45'),np.datetime64('2019-11-11T01:11:11')]))
+    assert_array_equal(timeseries_ds.band_0.coords['t'].data, np.asarray([ np.datetime64('2019-10-15T08:15:45'),np.datetime64('2019-11-11T01:11:11')]))
     timeseries_ds.band_0.sel(feature=1)
-    timeseries_ds.band_0.sel( time='2019-10-16')
+    timeseries_ds.band_0.sel( t='2019-10-16')
     print(timeseries_ds)
+    assert_array_equal( 4, timeseries_ds.band_0.sel(feature=1).sel( t="2019-10-15T08:15:45Z").data)
 
 
 def test_aggregate_polygon_result_nan_values(tmp_path):
@@ -50,11 +51,11 @@ def test_aggregate_polygon_result_nan_values(tmp_path):
 
 def test_aggregate_polygon_result_empty_ts_data(tmp_path):
     timeseries = {
+        "2019-04-01T12:34:56Z": [[], [7, 8]],
         "2019-01-01T12:34:56Z": [[1, 2], [3, 4]],
         "2019-02-01T12:34:56Z": [[5, 6], []],
-        "2019-03-01T12:34:56Z": [[], []],
-        "2019-04-01T12:34:56Z": [[], [7, 8]],
         "2019-05-01T12:34:56Z": [[9, 10], [11, 12]],
+        "2019-03-01T12:34:56Z": [[], []]
     }
     regions = GeometryCollection([
         Polygon([(0, 0), (5, 1), (1, 4)]),
@@ -67,7 +68,7 @@ def test_aggregate_polygon_result_empty_ts_data(tmp_path):
     filename = result.to_netcdf(tmp_path / 'timeseries_xarray_empty.nc')
     timeseries_ds = xr.open_dataset(filename)
 
-    assert_array_equal(timeseries_ds.time.data, [
+    assert_array_equal(timeseries_ds.t.data, [
         np.datetime64("2019-01-01T12:34:56Z"),
         np.datetime64("2019-02-01T12:34:56Z"),
         np.datetime64("2019-04-01T12:34:56Z"),
