30149a4f7bbe194495e535e6f0b588e5810ee7f0
Stefaan Lippens <stefaan.lippens@vito.be>
2022-02-16 14:42:43 +0100

    Reorganise unit test GeoJSON files a bit (EP-3981 related)

--- tests/test_views_execute.py
+++ tests/test_views_execute.py
@@ -726,7 +726,7 @@ def test_create_wmts_100(api100):
 
 
 def test_read_vector(api):
-    geometry_filename = str(get_path("GeometryCollection.geojson"))
+    geometry_filename = str(get_path("geojson/GeometryCollection01.json"))
     process_graph = api.load_json(
         "read_vector.json",
         preprocess=preprocess_check_and_replace("PLACEHOLDER", geometry_filename)
@@ -741,7 +741,7 @@ def test_read_vector(api):
 
 
 def test_read_vector_no_load_collection_spatial_extent(api):
-    geometry_filename = str(get_path("GeometryCollection.geojson"))
+    geometry_filename = str(get_path("geojson/GeometryCollection01.json"))
     preprocess1 = preprocess_check_and_replace("PLACEHOLDER", geometry_filename)
     preprocess2 = preprocess_regex_check_and_replace(r'"spatial_extent"\s*:\s*\{.*?\},', replacement='')
     process_graph = api.load_json(
@@ -773,7 +773,7 @@ def test_run_udf_on_vector(api100, udf_code):
     process_graph = {
         "geojson_file": {
             "process_id": "read_vector",
-            "arguments": {"filename": str(get_path("GeometryCollection.geojson"))},
+            "arguments": {"filename": str(get_path("geojson/GeometryCollection01.json"))},
         },
         "udf": {
             "process_id": "run_udf",
@@ -921,7 +921,7 @@ def test_process_reference_as_argument(api100):
 def test_load_collection_without_spatial_extent_incorporates_read_vector_extent(api):
     process_graph = api.load_json(
         "read_vector_spatial_extent.json",
-        preprocess=lambda s: s.replace("PLACEHOLDER", str(get_path("GeometryCollection.geojson")))
+        preprocess=lambda s: s.replace("PLACEHOLDER", str(get_path("geojson/GeometryCollection01.json")))
     )
     resp = api.check_result(process_graph)
     assert b'NaN' not in resp.data
@@ -936,7 +936,7 @@ def test_load_collection_without_spatial_extent_incorporates_read_vector_extent(
 def test_read_vector_from_feature_collection(api):
     process_graph = api.load_json(
         "read_vector_feature_collection.json",
-        preprocess=lambda s: s.replace("PLACEHOLDER", str(get_path("FeatureCollection.geojson")))
+        preprocess=lambda s: s.replace("PLACEHOLDER", str(get_path("geojson/FeatureCollection01.json")))
     )
     resp = api.check_result(process_graph)
     assert b'NaN' not in resp.data
@@ -967,7 +967,7 @@ def test_load_disk_data(api):
 def test_mask_with_vector_file(api):
     process_graph = api.load_json(
         "mask_with_vector_file.json",
-        preprocess=lambda s: s.replace("PLACEHOLDER", str(get_path("mask_polygons_3.43_51.00_3.46_51.02.json")))
+        preprocess=lambda s: s.replace("PLACEHOLDER", str(get_path("geojson/MultiPolygon02.json")))
     )
     api.check_result(process_graph)
 
@@ -2011,31 +2011,31 @@ def test_date_shift(api100, date, value, unit, expected):
             "Polygon", (5.09856905615526, 51.21910138452717, 5.111431257271536, 51.23089861521429),
     ),
     (
-            100, "meter", load_json("geojson/polygon01.json"),
+            100, "meter", load_json("geojson/Polygon01.json"),
             "Polygon", (5.0985703229820665, 51.19910120996896, 5.141430817509717, 51.23089799863948),
     ),
     (
-            -500, "meter", load_json("geojson/polygon01.json"),
+            -500, "meter", load_json("geojson/Polygon01.json"),
             "Polygon", (5.1084522374601145, 51.206647698484225, 5.129458164090344, 51.222687097228174),
     ),
     (
-            1, "kilometer", load_json("geojson/polygon01.json"),
+            1, "kilometer", load_json("geojson/Polygon01.json"),
             "Polygon", (5.087874843800581, 51.19113803453279, 5.154275007454256, 51.23894705603116),
     ),
     (
-            100, "meter", load_json("geojson/multipolygon01.json"),
+            100, "meter", load_json("geojson/MultiPolygon01.json"),
             "MultiPolygon", (5.098787598453318, 51.19911380673043, 5.141427521031006, 51.2408707459342),
     ),
     (
-            500, "meter", load_json("geojson/multipolygon01.json"),
+            500, "meter", load_json("geojson/MultiPolygon01.json"),
             "Polygon", (5.092851581512912, 51.19550604834508, 5.147157170251627, 51.244493919231424,)
     ),
     (
-            1000, "meter", load_json("FeatureCollection.geojson"),
+            1000, "meter", load_json("geojson/FeatureCollection01.json"),
             "FeatureCollection", (4.43568898, 51.09100882, 4.53429533, 51.20899105),
     ),
     (
-            1000, "meter", str(get_path("FeatureCollection.geojson")),
+            1000, "meter", str(get_path("geojson/FeatureCollection01.json")),
             "FeatureCollection", (4.43568898, 51.09100882, 4.53429533, 51.20899105),
     ),
 ])
