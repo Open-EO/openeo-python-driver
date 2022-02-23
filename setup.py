da953fcb2330e4fc119f6087fcf2b9613f0ae26a
Stefaan Lippens <stefaan.lippens@vito.be>
2022-02-23 11:48:42 +0100

    #105 pin down openeo_processes dependency
    openeo_processes has changed too much since 0.0.4 (without official releases since then), so a `>=0.0.4`  constraint is to much of a risk at this point

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         'werkzeug>=1.0.1',
         'requests',
         'openeo>=0.9.2.a1.dev',
-        'openeo_processes>=0.0.4',
+        'openeo_processes==0.0.4',
         'gunicorn>=20.0.1',
         'numpy>=1.17.0,<1.19.0',#tensorflow 2.3 requires numpy <1.19
         'shapely',
