0113c1f525dad0b0c060124ae475617d413ae095
Jan Van den bosch <jan@bossie.org>
2022-04-14 11:53:02 +0200

    EP-4118: clarify absolute "self" and "collection" relations
    https://github.com/radiantearth/stac-spec/blob/master/collection-spec/collection-spec.md#relation-types
    https://github.com/radiantearth/stac-spec/blob/master/item-spec/item-spec.md#relation-types

--- openeo_driver/views.py
+++ openeo_driver/views.py
@@ -836,7 +836,7 @@ def register_views_batch_jobs(
 
             links.append({
                 "rel": "self",
-                "href": url_for('.list_job_results', job_id=job_id, _external=True),
+                "href": url_for('.list_job_results', job_id=job_id, _external=True),  # MUST be absolute
                 "type": "application/json"
             })
             links.append({
@@ -970,11 +970,12 @@ def register_views_batch_jobs(
             "properties": properties,
             "links": [{
                 "rel": "self",
+                # MUST be absolute
                 "href": url_for('.get_job_result_item', job_id=job_id, item_id=item_id, _external=True),
                 "type": stac_item_media_type
             }, {
                 "rel": "collection",
-                "href": url_for('.list_job_results', job_id=job_id, _external=True),
+                "href": url_for('.list_job_results', job_id=job_id, _external=True),  # SHOULD be absolute
                 "type": "application/json"
             }],
             "assets": {
