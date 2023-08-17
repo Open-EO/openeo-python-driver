6aca4d89016071308175b0e2cc17ca1de1cd3678
Jeroen Verstraelen <jeroen.verstraelen@vito.be>
2023-08-17 17:36:54 +0200

    #423 add vector_to_raster process

--- openeo_driver/ProcessGraphDeserializer.py
+++ openeo_driver/ProcessGraphDeserializer.py
@@ -1657,6 +1657,30 @@ def raster_to_vector(args: Dict, env: EvalEnv):
     return image_collection.raster_to_vector()
 
 
+@non_standard_process(
+    ProcessSpec("vector_to_raster", description="Creates a raster cube as output based on a vector cube. The values in the output raster cube are based on the numeric properties in the input vector cube.")
+        .param('data', description="A vector data cube.", schema={"type": "object", "subtype": "vector-cube"})
+        .param('target_data_cube', description = "A raster data cube used as reference.", schema = {"type": "object", "subtype": "raster-cube"})
+        .returns("raster-cube", schema={"type": "object", "subtype": "raster-cube"})
+)
+def vector_to_raster(args: dict, env: EvalEnv) -> DriverDataCube:
+    input_vector_cube = extract_arg(args, "data")
+    target_data_cube = extract_arg(args, "target_data_cube")
+    if not isinstance(input_vector_cube, DriverVectorCube):
+        raise ProcessParameterInvalidException(
+            parameter="data",
+            process="vector_to_raster",
+            reason=f"Invalid data type {type(input_vector_cube)!r} expected vector-cube.",
+        )
+    if not isinstance(target_data_cube, DriverDataCube):
+        raise ProcessParameterInvalidException(
+            parameter="target_data_cube",
+            process="vector_to_raster",
+            reason=f"Invalid data type {type(target_data_cube)!r} expected raster-cube.",
+        )
+    return env.backend_implementation.vector_to_raster(input_vector_cube, target_data_cube)
+
+
 def _get_udf(args, env: EvalEnv) -> Tuple[str, str]:
     udf = extract_arg(args, "udf")
     runtime = extract_arg(args, "runtime")
