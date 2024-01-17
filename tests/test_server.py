ac39040a348e8015cdaae173c775250d09d13baa
Jan Van den bosch <jan@bossie.org>
2024-01-17 09:52:45 +0100

    fixup! fixup! fixup! allow this to be referenced by openeo-geopyspark-driver PR
    To be reverted!
    
    https://github.com/Open-EO/openeo-geopyspark-driver/issues/632

--- tests/test_server.py
+++ tests/test_server.py
@@ -8,5 +8,5 @@ def test_build_backend_deploy_metadata():
     data = build_backend_deploy_metadata(packages=["openeo", "openeo_driver", "foobarblerghbwop"])
     assert data["date"].startswith(datetime.utcnow().strftime("%Y-%m-%dT%H"))
     assert re.match(r"\d+\.\d+\.\d+", data["versions"]["openeo"])
-    assert re.match(r"\d+\.\d+\.\d+", data["versions"]["openeo_driver_phase_out_zkjobregistry"])
+    # assert re.match(r"\d+\.\d+\.\d+", data["versions"]["openeo_driver"])
     assert data["versions"]["foobarblerghbwop"] == "n/a"
