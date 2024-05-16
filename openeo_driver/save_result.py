be3d612591dedd0a19a53d0443e4d095c7b2686d
JeroenVerstraelen <verstraelenjeroen@gmail.com>
2024-05-16 11:23:28 +0200

    fix: set default output format for DelayedVector to json Open-EO/openeo-geopyspark-driver/#620

--- openeo_driver/save_result.py
+++ openeo_driver/save_result.py
@@ -904,12 +904,12 @@ def to_save_result(data: Any, format: Optional[str] = None, options: Optional[di
     elif isinstance(data, DriverVectorCube):
         return VectorCubeResult(cube=data, format=format, options=options)
     elif isinstance(data, DelayedVector):
-        if format is None or format.lower() == "geojson":
-            return JSONResult(data.geojson, format="geojson", options=options)
-        if format == "json":
+        if format is None or format == "json":
             # TODO #114 EP-3981 add vector cube support: keep features from feature collection
             geojsons = [mapping(geometry) for geometry in data.geometries_wgs84]
             return JSONResult(geojsons, format=format, options=options)
+        if format.lower() == "geojson":
+            return JSONResult(data.geojson, format="geojson", options=options)
         raise ValueError(f"Unsupported format {format} for DelayedVector")
     elif isinstance(data, DriverMlModel):
         return MlModelResult(ml_model = data)
