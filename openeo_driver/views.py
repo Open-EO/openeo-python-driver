6ef81e620812008a15b993b20d3b84d09dd60b9a
JeroenVerstraelen <verstraelenjeroen@gmail.com>
2024-06-09 10:27:22 +0200

    Revert "use exc_info field for exc_info_with_locals in sync requests"
    This reverts commit bcc138f439d039f220ac62341b3b368d6f277d0f.

--- openeo_driver/views.py
+++ openeo_driver/views.py
@@ -283,10 +283,10 @@ def register_error_handlers(app: flask.Flask, backend_implementation: OpenEoBack
         """Error handler for OpenEOApiException"""
         most_recent_exception = sys.exc_info()[1]
         fmt = Format(max_value_str_len=1000)
-        _log.error(log_message or repr(error), exc_info=True)
         _log.error(
-            "Sync request error stack trace with locals",
-            extra={"exc_info": format_exc(most_recent_exception, fmt=fmt)},
+            log_message or repr(error),
+            exc_info=True,
+            extra={"exc_info_with_locals": format_exc(most_recent_exception, fmt=fmt)},
         )
         error_dict = error.to_dict()
         return jsonify(error_dict), error.status_code
