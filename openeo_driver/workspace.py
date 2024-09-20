b2c20b5cc55baa41cb354c10be535a5fc3c63788
Jan Van den bosch <jan@bossie.org>
2024-09-20 10:01:04 +0200

    debug workspaces
    https://github.com/eu-cdse/openeo-cdse-infra/issues/264

--- openeo_driver/workspace.py
+++ openeo_driver/workspace.py
@@ -1,7 +1,11 @@
 import abc
+import logging
 import os.path
 import shutil
 from pathlib import Path
+
+
+_log = logging.getLogger(__name__)
 
 
 class Workspace(abc.ABC):
@@ -25,6 +29,9 @@ class DiskWorkspace(Workspace):
         target_directory.mkdir(parents=True, exist_ok=True)
         shutil.copy(file, target_directory)
 
+        _log.debug(f"copied {file.absolute()} to {target_directory}")
+
+
 class ObjectStorageWorkspace(Workspace):
     def __init__(self, bucket: str):
         self.bucket = bucket
@@ -35,7 +42,10 @@ class ObjectStorageWorkspace(Workspace):
         merge = os.path.normpath(merge)
         subdirectory = merge[1:] if merge.startswith("/") else merge
 
+        # TODO: openeo_driver should not reference openeo_geopyspark
         from openeogeotrellis.utils import s3_client
         s3_instance = s3_client()
-        s3_instance.upload_file(str(file), self.bucket, subdirectory + "/" + file.name)
+        key = subdirectory + "/" + file.name
+        s3_instance.upload_file(str(file), self.bucket, key)
 
+        _log.debug(f"uploaded {file.absolute()} to {self.bucket}/{key}")
