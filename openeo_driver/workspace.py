d6145108edfaae694229d0c7bdb83d76ec9ac5aa
Jan Van den bosch <jan@bossie.org>
2024-11-14 13:55:58 +0100

    merge collections
    https://github.com/Open-EO/openeo-geopyspark-driver/issues/677

--- openeo_driver/workspace.py
+++ openeo_driver/workspace.py
@@ -3,8 +3,10 @@ import logging
 import os.path
 import shutil
 from pathlib import Path, PurePath
+from typing import Optional
 
-from pystac import Collection, STACObject
+import pystac
+from pystac import Collection, STACObject, SpatialExtent, TemporalExtent
 from pystac.catalog import CatalogType
 from pystac.layout import TemplateLayoutStrategy
 
@@ -52,8 +54,6 @@ class DiskWorkspace(Workspace):
         raise NotImplementedError(f"importing objects is not supported yet")
 
     def merge_files(self, stac_resource: STACObject, target: PurePath, remove_original: bool = False):
-        # FIXME: merge new $stac_resource with the one in $this_workspace at $target
-        # FIXME: export STAC resources and assets underneath as well
         # FIXME: support remove_original and return equivalent workspace URIs (pass alternate_key and put workspace URIs in "alternate"?)
         target = os.path.normpath(target)
         target = Path(target[1:] if target.startswith("/") else target)
@@ -63,17 +63,96 @@ class DiskWorkspace(Workspace):
         target.parent.mkdir(parents=True, exist_ok=True)
 
         if isinstance(stac_resource, Collection):
+            new_collection = stac_resource
+
+            existing_collection = None
+            try:
+                existing_collection = pystac.Collection.from_file(str(target))
+            except FileNotFoundError:
+                pass  # nothing to merge into
+
             # collection ends up in file $target
             # items and assets and up in directory $target.parent
-            for item in stac_resource.get_items(recursive=True):
+            for item in new_collection.get_items(recursive=True):
                 for asset in item.assets.values():
                     shutil.copy(asset.href, target.parent)
                     asset.href = Path(asset.href).name
 
-            stac_resource.normalize_and_save(
+            (self
+             ._merge_collections(existing_collection, new_collection)
+             .normalize_and_save(
                 root_href=str(target.parent),
                 catalog_type=CatalogType.SELF_CONTAINED,
-                strategy=TemplateLayoutStrategy(collection_template=target.name, item_template="${id}.json"),
-            )
+                strategy=TemplateLayoutStrategy(collection_template=target.name, item_template="${id}.json")
+             ))
         else:
             raise NotImplementedError(stac_resource)
+
+    def _merge_collections(self, existing_collection: Optional[Collection], new_collection: Collection) -> Collection:
+        if existing_collection:
+            existing_collection.extent.spatial = self._merge_spatial_extents(
+                existing_collection.extent.spatial,
+                new_collection.extent.spatial
+            )
+
+            existing_collection.extent.temporal = self._merge_temporal_extents(
+                existing_collection.extent.temporal,
+                new_collection.extent.temporal
+            )
+
+            for new_item in new_collection.get_items(recursive=True):
+                if existing_collection.get_item(new_item.id, recursive=True):
+                    raise ValueError(f"item {new_item.id} is already in collection {existing_collection.id}")
+
+                existing_collection.add_item(new_item, strategy=TemplateLayoutStrategy(item_template="${id}.json"))
+                return existing_collection
+
+        return new_collection
+
+    def _merge_spatial_extents(self, a: SpatialExtent, b: SpatialExtent) -> SpatialExtent:
+        overall_bbox_a, *sub_bboxes_a = a.bboxes
+        overall_bbox_b, *sub_bboxes_b = b.bboxes
+
+        merged_overall_bbox = [
+            min(overall_bbox_a[0], overall_bbox_b[0]),
+            min(overall_bbox_a[1], overall_bbox_b[1]),
+            max(overall_bbox_a[2], overall_bbox_b[2]),
+            max(overall_bbox_a[3], overall_bbox_b[3])
+        ]
+
+        merged_sub_bboxes = sub_bboxes_a + sub_bboxes_b
+
+        merged_spatial_extent = SpatialExtent([merged_overall_bbox])
+        if merged_sub_bboxes:
+            merged_spatial_extent.bboxes.append(merged_sub_bboxes)
+
+        return merged_spatial_extent
+
+    def _merge_temporal_extents(self, a: TemporalExtent, b: TemporalExtent) -> TemporalExtent:
+        overall_interval_a, *sub_intervals_a = a.intervals
+        overall_interval_b, *sub_intervals_b = b.intervals
+
+        def min_time(t1: Optional[str], t2: Optional[str]) -> Optional[str]:
+            if t1 is None or t2 is None:
+                return None
+
+            return min(t1, t2)
+
+        def max_time(t1: Optional[str], t2: Optional[str]) -> Optional[str]:
+            if t1 is None or t2 is None:
+                return None
+
+            return max(t1, t2)
+
+        merged_overall_interval = [
+            min_time(overall_interval_a[0], overall_interval_b[0]),
+            max_time(overall_interval_a[1], overall_interval_b[1])
+        ]
+
+        merged_sub_intervals = sub_intervals_a + sub_intervals_b
+
+        merged_temporal_extent = TemporalExtent([merged_overall_interval])
+        if merged_sub_intervals:
+            merged_temporal_extent.intervals.append(merged_sub_intervals)
+
+        return merged_temporal_extent
--- tests/test_workspace.py
+++ tests/test_workspace.py
@@ -1,5 +1,6 @@
 import datetime as dt
 from pathlib import Path
+from typing import List
 
 from pystac import Asset, Collection, Extent, Item, SpatialExtent, TemporalExtent
 import pytest
@@ -53,10 +54,11 @@ def test_disk_workspace_remove_original(tmp_path, remove_original):
 def test_merge_from_disk_new(tmp_path):
     source_directory = tmp_path / "src"
     source_directory.mkdir()
-    source_asset_file = source_directory / "asset.tif"
-    source_asset_file.touch()
 
-    new_stac_collection = _collection(source_asset_file)
+    asset_file = source_directory / "asset.tif"
+    asset_file.touch()
+
+    new_stac_collection = _collection("collection", [asset_file])
 
     target = Path("path") / "to" / "collection.json"
 
@@ -69,32 +71,79 @@ def test_merge_from_disk_new(tmp_path):
     assert merged_stac_collection.validate_all() == 1
     # TODO: check Collection
 
-    assets = [
-        asset for item in merged_stac_collection.get_items(recursive=True) for asset in item.get_assets().values()
-    ]
-
-    assert len(assets) == 1
-
-    for asset in assets:
-        asset.copy(str(tmp_path / Path(asset.href).name))  # downloads the asset file
+    assert _download_assets(merged_stac_collection, target_dir=tmp_path) == 1
 
 
-def _collection(asset_file: Path) -> Collection:
-    collection = Collection(
-        id="somecollection",
-        description="some description",
-        extent=Extent(spatial=SpatialExtent([[-180, -90, 180, 90]]), temporal=TemporalExtent([[None, None]])),
+def test_merge_from_disk_into_existing(tmp_path):
+    source_directory = tmp_path / "src"
+    source_directory.mkdir()
+
+    asset_file1 = source_directory / "asset1.tif"
+    asset_file1.touch()
+
+    asset_file2 = source_directory / "asset2.tif"
+    asset_file2.touch()
+
+    existing_stac_collection = _collection(
+        "existing_collection",
+        [asset_file1],
+        spatial_extent=SpatialExtent([[0, 50, 2, 52]]),
+        temporal_extent=TemporalExtent([[dt.datetime.fromisoformat("2024-11-01T00:00:00+00:00"),
+                                         dt.datetime.fromisoformat("2024-11-03T00:00:00+00:00")]])
+    )
+    new_stac_collection = _collection(
+        "new_collection",
+        [asset_file2],
+        spatial_extent=SpatialExtent([[1, 51, 3, 53]]),
+        temporal_extent=TemporalExtent([[dt.datetime.fromisoformat("2024-11-02T00:00:00+00:00"),
+                                         dt.datetime.fromisoformat("2024-11-04T00:00:00+00:00")]])
     )
 
-    item_id = asset_key = asset_file.name
+    target = Path("path") / "to" / "collection.json"
 
-    item = Item(id=item_id, geometry=None, bbox=None, datetime=dt.datetime.utcnow(), properties={})
-    item.add_asset(key=asset_key, asset=Asset(href=str(asset_file)))
+    workspace = DiskWorkspace(root_directory=tmp_path)
+    workspace.merge_files(stac_resource=existing_stac_collection, target=target)
+    workspace.merge_files(stac_resource=new_stac_collection, target=target)
 
-    collection.add_item(item)
+    workspace_dir = (workspace.root_directory / target).parent
+
+    merged_stac_collection = Collection.from_file(str(workspace_dir / "collection.json"))
+    assert merged_stac_collection.validate_all() == 2
+
+    assert _download_assets(merged_stac_collection, target_dir=tmp_path) == 2
+
+    assert merged_stac_collection.extent.spatial.bboxes == [[0, 50, 3, 53]]
+    assert merged_stac_collection.extent.temporal.intervals == [
+        [dt.datetime.fromisoformat("2024-11-01T00:00:00+00:00"),
+         dt.datetime.fromisoformat("2024-11-04T00:00:00+00:00")]
+    ]
+
+
+def _collection(collection_id: str,
+                asset_files: List[Path],
+                spatial_extent: SpatialExtent = SpatialExtent([[-180, -90, 180, 90]]),
+                temporal_extent: TemporalExtent = TemporalExtent([[None, None]])) -> Collection:
+    collection = Collection(
+        id=collection_id,
+        description=collection_id,
+        extent=Extent(spatial=spatial_extent, temporal=temporal_extent),
+    )
+
+    for asset_file in asset_files:
+        item_id = asset_key = asset_file.name
+
+        item = Item(id=item_id, geometry=None, bbox=None, datetime=dt.datetime.utcnow(), properties={})
+        item.add_asset(key=asset_key, asset=Asset(href=str(asset_file)))
+
+        collection.add_item(item)
 
     return collection
 
 
-def skip_merge_into_existing():
-    raise NotImplementedError
+def _download_assets(collection: Collection, target_dir: Path) -> int:
+    assets = [asset for item in collection.get_items(recursive=True) for asset in item.get_assets().values()]
+
+    for asset in assets:
+        asset.copy(str(target_dir / Path(asset.href).name))  # downloads the asset file
+
+    return len(assets)
