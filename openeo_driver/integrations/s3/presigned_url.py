3578c4f3e83ae02bfe803631ab9f6aea5e4d070a
Peter Van Bouwel <peter.vanbouwel@vito.be>
2025-05-22 15:51:26 +0200

    refactor: separate function for default None presigned url

--- openeo_driver/integrations/s3/presigned_url.py
+++ openeo_driver/integrations/s3/presigned_url.py
@@ -10,7 +10,22 @@ if TYPE_CHECKING:
 _log = logging.getLogger(__name__)
 
 
-def create_presigned_url(client: S3Client, bucket_name: str, object_name: str, expiration: int = 3600) -> Optional[str]:
+def create_presigned_url(client: S3Client, bucket_name: str, object_name: str, expiration: int = 3600) -> str:
+    """
+    Generate a presigned URL to share an S3 object
+
+    :return: Presigned URL as string.
+    """
+    return client.generate_presigned_url(
+        "get_object",
+        Params={"Bucket": bucket_name, "Key": object_name},
+        ExpiresIn=expiration,
+    )
+
+
+def attempt_create_presigned_url(
+    client: S3Client, bucket_name: str, object_name: str, expiration: int = 3600
+) -> Optional[str]:
     from botocore.exceptions import ClientError
     """
     Generate a presigned URL to share an S3 object
@@ -18,14 +33,7 @@ def create_presigned_url(client: S3Client, bucket_name: str, object_name: str, e
     :return: Presigned URL as string. If error, returns None.
     """
     try:
-        response = client.generate_presigned_url(
-            "get_object",
-            Params={"Bucket": bucket_name, "Key": object_name},
-            ExpiresIn=expiration,
-        )
+        return create_presigned_url(client, bucket_name=bucket_name, object_name=object_name, expiration=expiration)
     except ClientError as e:
         logging.info(f"Failed to create presigned url: {e}, continuing without.")
         return None
-
-    # The response contains the presigned URL
-    return response
