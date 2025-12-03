18448603c9b41b3cecabcdb177f7e8986cc75821
Emile Sonneveld <emile.sonneveld@vito.be>
2025-12-03 23:35:20 +0100

    Fix for relative and absolute paths in stac. https://github.com/cloudinsar/s1-workflows/issues/70

--- openeo_driver/workspace.py
+++ openeo_driver/workspace.py
@@ -44,16 +44,20 @@ class DiskWorkspace(Workspace):
 
     def import_file(self, common_path: Union[str, Path], file: Path, merge: str, remove_original: bool = False) -> str:
         subdirectory = remove_slash_prefix(os.path.normpath(merge))
-        file_relative = file.relative_to(common_path)
+        if file.is_absolute():
+            file_relative = file.relative_to(common_path)
+        else:
+            file_relative = file
+        file_absolute = Path(common_path) / file_relative
         target_directory = self.root_directory / subdirectory / file_relative.parent
         target_directory.relative_to(self.root_directory)  # assert target_directory is in root_directory
 
         target_directory.mkdir(parents=True, exist_ok=True)
 
         operation = shutil.move if remove_original else shutil.copy
-        operation(str(file), str(target_directory))
+        operation(str(file_absolute), str(target_directory))
 
-        _log.debug(f"{'moved' if remove_original else 'copied'} {file.absolute()} to {target_directory}")
+        _log.debug(f"{'moved' if remove_original else 'copied'} {file_absolute} to {target_directory}")
         return f"file:{target_directory / file.name}"
 
     def import_object(self, common_path: str, s3_uri: str, merge: str, remove_original: bool = False):
