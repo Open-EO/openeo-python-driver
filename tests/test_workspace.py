7c192717444046b6dfb598ea8c7223282ccf97c0
Emile Sonneveld <emile.sonneveld@vito.be>
2025-12-04 00:03:16 +0100

    fix

--- tests/test_workspace.py
+++ tests/test_workspace.py
@@ -39,17 +39,18 @@ def test_disk_workspace_relative(tmp_path):
     merge = "subdirectory"
     source_directory = tmp_path / "src"
     source_directory.mkdir()
-    source_file = Path("file")
-    (source_directory / "file").touch()
+    source_file_relative = Path("file")
+    source_file_absolute = source_directory / "file"
+    source_file_absolute.touch()
 
     subdirectory = merge[1:] if merge.startswith("/") else merge
     target_directory = tmp_path / subdirectory
 
     workspace = DiskWorkspace(root_directory=tmp_path)
-    workspace.import_file(common_path=source_directory, file=source_file, merge=merge)
+    workspace.import_file(common_path=source_directory, file=source_file_relative, merge=merge)
 
-    assert (target_directory / source_file.name).exists()
-    assert source_file.exists()
+    assert (target_directory / source_file_absolute.name).exists()
+    assert source_file_absolute.exists()
 
 
 @pytest.mark.parametrize("remove_original", [False, True])
