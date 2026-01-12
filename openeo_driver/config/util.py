a6c45e6504eda656907263e3890421524545ce80
Stefaan Lippens <stefaan.lippens@vito.be>
2026-01-12 15:12:51 +0100

    fine-tune openeo_driver.config.util.Exclude

--- openeo_driver/config/util.py
+++ openeo_driver/config/util.py
@@ -3,6 +3,10 @@ from typing import Container, Callable
 
 
 class Exclude(Container[str]):
+    """
+    Exclude list, implementing the `Container` interface,
+    based on a callable (instead of an exhausive listing).
+    """
     def __init__(self, exclude: Callable[[str], bool]):
         self.exclude = exclude
 
@@ -11,4 +15,14 @@ class Exclude(Container[str]):
 
     @classmethod
     def by_prefix(cls, prefix: str) -> Exclude:
-        return cls(lambda s: isinstance(s, str) and s.startswith(prefix))
+        return cls(exclude=lambda s: isinstance(s, str) and s.startswith(prefix))
+
+    def union(self, other: Container[str]) -> Exclude:
+        """
+        Combine with another exclude list in logical OR fashion:
+        an item is considered excluded when it is excluded by either this or the other exclude list.
+
+        Note that ther other exclude list does not have to be an Exclude instance,
+        it just has to implement the `Container` interface like a list or set.
+        """
+        return Exclude(exclude=lambda s: s in self or s in other)
