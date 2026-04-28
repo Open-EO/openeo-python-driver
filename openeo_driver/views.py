87d718b3fb8bb30b48a9898892f8275149a28ff1
Emile Sonneveld <emile.sonneveld@vito.be>
2026-04-28 13:24:40 +0200

    Better log on error when asset not found.

--- openeo_driver/views.py
+++ openeo_driver/views.py
@@ -1414,7 +1414,9 @@ def register_views_batch_jobs(
                                 f"{filename!r} not in {list(results.keys())}, nor in {file_paths}"
                             )
                     except Exception as e:
-                        _log.warning(f"Could not get file paths from {link['href']}: {e}")
+                        _log.error(repr(e), exc_info=True)
+                        _log.error(repr(type(e)), exc_info=True)
+                        _log.error(f"Could not get file paths from {link['href']}: {e}")
                 if not result:
                     raise FilePathInvalidException(f"{filename!r} not in {list(results.keys())}")
         if result.get("href", "").startswith("s3://"):
