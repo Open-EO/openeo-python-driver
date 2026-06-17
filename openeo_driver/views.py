9c6f79152c484ce04603861b9dafce82196e1256
Emile Sonneveld <emile.sonneveld@vito.be>
2026-06-17 10:51:50 +0200

    fix

--- openeo_driver/views.py
+++ openeo_driver/views.py
@@ -1497,13 +1497,9 @@ def register_views_batch_jobs(
         try:
             user_id = user_id_b64_decode(user_base64)
         except (binascii.Error, UnicodeDecodeError):
-            user_id = None
-        if user_id is None or not is_secure_key(secure_key):
             full_item_id = "/".join([user_base64, secure_key, item_id])
             user = auth_handler.get_user_from_bearer_token(request)
             return _get_job_result_item(job_id, full_item_id, user.user_id)
-        if not is_secure_key(secure_key):
-            raise CredentialsInvalidException()
         expires = request.args.get('expires')
         signer = get_backend_config().url_signer
         signer.verify_job_item(signature=secure_key, job_id=job_id, user_id=user_id, item_id=item_id, expires=expires)
@@ -1515,13 +1511,9 @@ def register_views_batch_jobs(
         try:
             user_id = user_id_b64_decode(user_base64)
         except (binascii.Error, UnicodeDecodeError):
-            user_id = None
-        if user_id is None or not is_secure_key(secure_key):
             full_item_id = "/".join([user_base64, secure_key, item_id])
             user = auth_handler.get_user_from_bearer_token(request)
             return _get_job_result_item11(job_id, full_item_id, user.user_id)
-        if not is_secure_key(secure_key):
-            raise CredentialsInvalidException()
         expires = request.args.get('expires')
         signer = get_backend_config().url_signer
         signer.verify_job_item(signature=secure_key, job_id=job_id, user_id=user_id, item_id=item_id, expires=expires)
@@ -1951,13 +1943,9 @@ def register_views_batch_jobs(
         try:
             user_id = user_id_b64_decode(user_base64)
         except (binascii.Error, UnicodeDecodeError):
-            user_id = None
-        if user_id is None or not is_secure_key(secure_key):
             full_filename = "/".join([user_base64, secure_key, filename])
             user = auth_handler.get_user_from_bearer_token(request)
             return _download_job_result(job_id=job_id, filename=full_filename, user_id=user.user_id)
-        if not is_secure_key(secure_key):
-            raise CredentialsInvalidException()
         expires = request.args.get('expires')
         signer = get_backend_config().url_signer
         signer.verify_job_asset(
