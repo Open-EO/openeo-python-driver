85f48d09e2a333194430bb86215db5f5abf9f0a1
Jeroen Dries <jeroen.dries@vito.be>
2026-09-24 22:16:27 +0200

    extra whitelist of rename_labels and apply for data mask

--- openeo_driver/processgraph/evaluator.py
+++ openeo_driver/processgraph/evaluator.py
@@ -327,6 +327,8 @@ def check_subgraph_for_data_mask_optimization(args: dict) -> bool:
         "filter_temporal",
         "load_collection",
         "resample_spatial",
+        "rename_labels",
+        "apply",
     }
 
     children_node_types = flatten_children_node_types(args["data"])
